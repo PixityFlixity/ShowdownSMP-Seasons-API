f919f56f22db8a80153620ec23ad31615688e599
Pixity <81818297+PixityFlixity@users.noreply.github.com>
2024-11-30 18:48:22 -0500

    Season Testing
    Changed every season to "Winter" to test for production setting.

--- main.py
+++ main.py
@@ -15,11 +15,11 @@ def get_current_season() -> str:
     if month in (12, 1, 2):
         return "Winter"
     elif month in (3, 4, 5):
-        return "Spring"
+        return "Winter"
     elif month in (6, 7, 8):
-        return "Summer"
+        return "Winter"
     elif month in (9, 10, 11):
-        return "Autumn"
+        return "Winter"
 
 @app.get("/")
 def read_root():
